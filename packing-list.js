9042e607508071873bb24dc08c192c242bac00cf
kmomozin <khinmomozin@gmail.com>
2022-12-08 10:52:41 -0700

    Iterating over array

---
+++ packing-list.js
@@ -0,0 +1,12 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+let index = 0;
+while (index < packingList.length) {
+  console.log(packingList[index]);
+  index++;
+}
